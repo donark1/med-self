120b4429f8431e24e7a0081310e82718a7b7981e
donark1 <don_ark@sbcglobal.net>
2021-02-06 10:46:01 -0800

    Med-Self Database creation with dummy data

---
+++ database/data.sql
@@ -0,0 +1,31 @@
+insert into "users"
+  ( "FirstName", "LastName", "Email", "Password", "Gender", "DateOfBirth", "City", "ZipCode", "Height(inches)", "Weight(lbs)")
+  values
+    ('Joe', 'Schmoe', 'jschmoe@example.com', 'abcde', 'Male', '1/1/1940', 'Irvine', '92618', '70', '210'),
+    ('Jane', 'Doe', 'jdoe@example.com', '12345', 'Female', '6/30/1970', 'Newport Beach','92660', '55', '110');
+
+insert into "bodyParts"
+  ("name")
+  values
+    ('Head/Neck'),
+    ('Shoulders/Arms/Hands'),
+    ('Chest'),
+    ('Abdomen'),
+    ('Legs/Feet');
+
+insert into "symptoms"
+  ("userId", "bodyPartId", "description")
+  values
+  (1, 4, 'lower backache'),
+  (2, 3, 'chest pain');
+
+insert into "results"
+  ("symptomId", "userForumDiagnosis")
+  values
+  (1, 'Lumbar Strain'),
+  (1, 'Lumbar Herniated Disc'),
+  (1, 'Vertebral Compression Fracture'),
+  (1, 'Osteoarthritis'),
+  (2, 'Heart Attack'),
+  (2, 'Fractured Ribs'),
+  (2, 'Heartburn');
--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,79 @@ set client_min_messages to warning;
 
 -- DANGER: this is NOT how to do it in the real world.
 -- `drop schema` INSTANTLY ERASES EVERYTHING.
-drop schema "public" cascade;
+drop schema "med-self" cascade;
 
-create schema "public";
+create schema "med-self";
+
+
+ CREATE TABLE "users" (
+	"userId" serial NOT NULL,
+	"FirstName" TEXT NOT NULL,
+	"LastName" TEXT NOT NULL,
+	"Email" TEXT NOT NULL,
+	"Password" TEXT NOT NULL,
+	"Gender" TEXT NOT NULL,
+	"DateOfBirth" DATE NOT NULL,
+	"City" TEXT NOT NULL,
+	"ZipCode" integer NOT NULL,
+	"Height(inches)" integer NOT NULL,
+	"Weight(lbs)" integer NOT NULL,
+	"isGuest" BOOLEAN NOT NULL DEFAULT 'true',
+	"createdAt" TIMESTAMP NOT NULL DEFAULT 'now()',
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "bodyParts" (
+	"bodyPartId" serial NOT NULL,
+	"name" TEXT NOT NULL,
+	CONSTRAINT "bodyParts_pk" PRIMARY KEY ("bodyPartId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "symptoms" (
+	"symptomId" serial NOT NULL,
+	"bodyPartId" serial NOT NULL,
+	"userId" serial NOT NULL,
+	"description" TEXT DEFAULT NULL,
+	CONSTRAINT "symptoms_pk" PRIMARY KEY ("symptomId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "results" (
+	"userForumDiagnosis" TEXT NOT NULL,
+	"symptomId" integer NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "userAccess" (
+	"bodyPartId" integer NOT NULL,
+	"resultsId" integer NOT NULL,
+	"userId" integer NOT NULL,
+	"resources" TEXT NOT NULL,
+	"medicalFacilitiesNearBy" TEXT NOT NULL,
+	"userQuestionForPro" TEXT NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE "symptoms" ADD CONSTRAINT "symptoms_fk0" FOREIGN KEY ("bodyPartId") REFERENCES "bodyParts"("bodyPartId");
+ALTER TABLE "symptoms" ADD CONSTRAINT "symptoms_fk1" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+
+ALTER TABLE "results" ADD CONSTRAINT "results_fk1" FOREIGN KEY ("symptomId") REFERENCES "symptoms"("symptomId");
