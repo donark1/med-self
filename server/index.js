568f08bc2f11dc20935db09519855ebbd4b14bc6
donark1 <don_ark@sbcglobal.net>
2021-04-20 21:21:24 -0700

    deployment-preparation

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const { Pool } = require('pg');
+const errorMiddleware = require('./error-middleware');
 const ClientError = require('./client-error'); // eslint-disable-line
 
 const db = new Pool({
@@ -418,6 +419,14 @@ app.post('/api/treatments/legsfeettreatments', (req, res, next) => {
     })
     .catch(err => next(err));
 });
+
+app.use((req, res) => {
+  res.sendFile('/index.html', {
+    root: path.join(__dirname, 'public')
+  });
+});
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
